bd7a6186832c4b4197bad8f2274a9bf260856415
Thorvald van Beek <tvanbeek@roc-dev.com>
2019-12-10 13:10:04 +0100

    Uitwerking van de exercise

--- error_log.php
+++ error_log.php
@@ -8,15 +8,25 @@ function countdown($input){
     if ($input < 0 || $input > 10) {
         error_log("Error 579: Enkel getallen tussen de 0 en 10 worden geaccepteerd" . PHP_EOL , 3, "error.log");
         
-        echo "NO";
+        echo "NO, probeer onder de 10 te blijven.";
 
         exit;
     } 
+       
+       if ($input == NULL){ 
+        error_log("Error 580: Veld kan niet leeg zijn" . PHP_EOL , 3, "error.log");
+
+        echo "Veld kan niet leeg zijn";
+
+        exit;
+       }
+
+        if($input >= 0 || $input <= 10 || $input !== NULL){
 
 for ($i=$input; $i >= 0; $i--) { 
     echo $i . " Seconden" . PHP_EOL;
 }
-     
+}
 }
 
 try{
@@ -28,5 +38,5 @@ catch(exception $error){
     echo 'Fout gevonden: '.$error->getMessage();
     exit;
 }
-echo $input . ' seconden afgeteld';
+echo 'Vanaf ' . $input . ' seconden afgeteld';
 exit;
